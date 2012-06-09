7ee665593118d24944e4f07594e418371f3989b6
unknown <howard@howard2.redmond.corp.microsoft.com>
2012-06-08 18:05:57 -0700

    moved state mod functions out of javascript object definitions and into module scope

--- RestBugs-Node/app.js
+++ RestBugs-Node/app.js
@@ -2,7 +2,8 @@ var express = require('express');
 
 var databaseUrl = "restbugs";
 var collections = ["bugs"];
-var db = require('mongojs').connect(databaseUrl, collections);
+var mongo = require('mongojs');
+var db = mongo.connect(databaseUrl, collections);
 
 var app = express.createServer();
 app.listen(9200);
@@ -15,7 +16,7 @@ app.configure(function(){
 	app.use(express.bodyParser());	//where is app.use defined and what does it do??
 });
 
-function Bug(config) {
+/*function Bug(config) {
 	var self = this;		// this seems to be necessary so that updateStatus works - WHY???
 	
 	// I *think* this is the right way to make privately scoped functions for the Bug object
@@ -67,14 +68,60 @@ function Bug(config) {
 		this.status = config.jsonObj.status;
 		this.history = config.jsonObj.history===undefined ? [] : config.jsonObj.history;
 	}
+};*/
+
+function addToHistory(doc, comments, stateChanges){
+	if(doc.history === undefined)
+		doc.history = [];
+
+	doc.history.push({
+		addedOn: new Date(),
+		comments: comments,
+		changes: stateChanges
+	});
 };
+
+function updateStatus(doc, status, comments){
+	doc.status = status;
+	addToHistory(doc, comments, {status : doc.status});
+};
+
+function activate(doc, comments){
+	updateStatus(doc, 'Working', comments);
+};
+
+function resolve(doc, comments){
+	updateStatus(doc, 'QA', comments);
+};
+
+function close(doc, comments){
+	updateStatus(doc, 'Done', comments);
+};
+
+function toBacklog(doc, comments){
+	updateStatus(doc, 'Backlog', comments);
+};
+
+function newbug(title, description){
+	var bug = {		
+		title: title,
+		description: description
+	};
+	toBacklog(bug, "bug created");
+	return bug;
+};
+
+
+
+
+
 
 app.get('/bugs', function(req, res){
 	res.render('bugs-all.html', { title: "Bugs API root"});
 });
 
 app.get('/bugs/backlog', function(req, res){
-	db.bugs.find({status:'Backlog'}, function(err, docs){
+	db.bugs.find( {status: 'Backlog'}, function(err, docs) {
 		res.render('bugs-all.html', { 
 			title: "Backlog", 
 			model: { bugs : docs }});	
@@ -84,36 +131,17 @@ app.get('/bugs/backlog', function(req, res){
 app.post('/bugs/backlog', function(req, res){
 	//this could be either a create or an update
 	//TODO: replace with upsert-style call
-	var id = req.body.id;
-	
-	if(id===undefined){
-		//create a new bug
-		console.log('create a new bug');
-		db.bugs.save(new Bug({title: req.body.title, description: req.body.description}), function(err, savedDoc){
-			db.bugs.find({status:'Backlog'}, function(err, docs){
-				res.header('Location', req.headers.host + '/bugs/' + savedDoc._id);
-				res.render('bugs-all.html', { 
-				status: 201,
-				title: "Backlog", 
-				model: { bugs : docs }});	
-			});
-		});
-	}
-	else
-	{
-		//update
-		console.log('move bug ' + id + ' to backlog');
-
-		db.bugs.find({_id:id}, function(err, doc){
-			doc.toBacklog(req.body.comments);
-
-			db.bugs.update({_id:id}, doc, function(err, updatedDoc){
-				db.bugs.find({status:'Backlog'}, function(err, docs){
-					res.render('bugs-all.html', { 
-					title: "Backlog", 
-					model: { bugs : docs }});	
+	if(req.body.id===undefined) {
+		console.log('add a new bug');
+		db.bugs.save(
+			newbug(req.body.title, req.body.description), 
+			function(err, savedDoc) {
+				db.bugs.find( {status: 'Backlog'}, function(err, docs) {
+					res.render('bugs-all.html', {
+						title: 'Backlog',
+						model: docs
+					});
 				});
-			});
 		});
 	}
 });
@@ -127,12 +155,13 @@ app.get('/bugs/working', function(req, res){
 });
 
 app.post('/bugs/working', function(req, res){
-	db.bugs.find({_id:req.body.id}, function(err, doc){
-		var b = new Bug({jsonObj: doc});
-		
-		b.activate(req.body.comments);
+	db.bugs.findOne( {_id: mongo.ObjectId(req.body.id) }, function(err, doc) {
+		//todo: return 404 if doc is undefined
 
-		db.bugs.update({_id:req.body.id}, b, function(err, updatedDoc){
+		//update the doc
+		updateStatus(doc, 'Working', req.body.comments);
+
+		db.bugs.update( {_id: mongo.ObjectId(req.body.id) }, doc, function(err, updatedDoc){
 			db.bugs.find({status:'Working'}, function(err, docs){
 				res.render('bugs-all.html', { 
 				title: "Working", 
@@ -188,7 +217,3 @@ app.post('/bugs/done', function(req, res){
 
 // first, let's remove any initial values in the database
 db.bugs.remove({});
-
-// let's initialize the database with some bugs
-db.bugs.save(new Bug({title: "first bug", description: "a bug description"}));
-db.bugs.save(new Bug({title: "second bug", description: "another bug description"}));
