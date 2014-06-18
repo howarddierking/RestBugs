6eb9c74648bbc5dfc8fde776647f1cd453bd2e2b
Howard Dierking <howarddierking@gmail.com>
2014-06-18 15:48:03 -0700

    fixed #16

--- RestBugs-Node/app.js
+++ RestBugs-Node/app.js
@@ -13,6 +13,28 @@ app.set('view engine', 'ejs');
 app.set('view options', {
 	layout: false
 });
+
+// middleware
+var contentExtensions = function(req, res, next){
+	debugger;
+	var m = req.url.match(/\.(?:json|html)$/i);
+	if(m && m.length > 0){
+		var extension = m[0];
+		switch(extension){
+			case '.json':
+				req.headers.accept = 'application/json';
+				break;
+			case '.html':
+				req.headers.accept = 'text/html';
+				break;
+		}
+		req.url = req.url.substring(0, req.url.lastIndexOf('.'));
+	} 
+	
+	next();
+};
+
+app.use(contentExtensions);
 app.use(express.bodyParser());
 app.use(app.router);
 
@@ -22,10 +44,8 @@ app.get('/bugs/backlog', routes.backlog);
 app.post('/bugs/backlog', routes.backlog_post);
 app.get('/bugs/working', routes.working);
 app.post('/bugs/working', routes.working_post);
-
 app.get('/bugs/qa', routes.qa);
 app.post('/bugs/qa', routes.qa_post);
-
 app.get('/bugs/done', routes.done);
 app.post('/bugs/done', routes.done_post);
 
@@ -33,3 +53,4 @@ app.post('/bugs/done', routes.done_post);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
--- RestBugs-Node/routes/index.js
+++ RestBugs-Node/routes/index.js
@@ -3,30 +3,61 @@ var bugs = require('../lib/bugsdata'),
 	_ = require('underscore'),
 	b = require('../lib/bugs'),
 	util = require('util'),
-	assert = require('assert');
+	assert = require('assert'),
+	headerLinking = require('web-linking').httpheader;
+
+var _toAbsoluteUrl = function(host, rootRelativeUrl, extension){
+	return 'http://' + host + rootRelativeUrl + (extension || '');
+};
+
+var _getAbsoluteUrlResolver = function(req){
+	return function(rootRelativeUrl, extension){
+		return _toAbsoluteUrl(req.headers.host, rootRelativeUrl, extension);
+	}
+};
 
 // render helper
 function renderView(res, title, model) {
-	debugger;
+	var absoluteUrlResolver = _getAbsoluteUrlResolver(res.req);
 
 	res.set('Cache-Control', 'no-cache, no-store');
+
+	var links = [
+	{
+		rel: 'alternate',
+		href: absoluteUrlResolver(res.req.url + '.json'),
+		type: 'application/json',
+		title: 'JSON format'
+	},
+	{
+		rel: 'alternate',
+		href: absoluteUrlResolver(res.req.url + '.html'),
+		type: 'text/html',
+		title: 'HTML format'
+	}];
+	// using this because the default link function in the ExpressJS Response object 
+	// does not support adding parameters beyond rel
+	headerLinking.setToResponse(res, links);
 
 	var viewModel = {
 		title: title,
 		model: model,
-		toAbsoluteUrl: function(rootRelativeUrl){
-			return 'http://' + res.req.headers.host + rootRelativeUrl;
-		}
+		toAbsoluteUrl: absoluteUrlResolver
 	}
+	
+	// use the built in content negotiation to select a format template
 	res.format({
 		'text/html': function() {
-			res.render('bugs-all-html', viewModel);				
+			viewModel.formatExtension = '.html';
+			res.render('bugs-all-html', viewModel);	
 		},
 
 		'application/json': function() {
-			res.render('bugs-all-json', viewModel);				
+			viewModel.formatExtension = '.json';
+			res.render('bugs-all-json', viewModel);	
 		}
 	});
+	
 };
 
 var listView = function(res, title){
@@ -46,7 +77,6 @@ exports.backlog = function(req, res){
 };
 
 exports.backlog_post = function(req, res){
-debugger;
 	var bugID = req.body.id,
 		title = req.body.title,
 		description = req.body.description,
@@ -86,8 +116,6 @@ exports.working_post = function(req, res){
 	bugs.getBug(bugID, function(err, data){
 		if(err)
 			return res.send(500, err);
-
-debugger;
 
 		data.working(comments);
 		bugs.saveBug(data, function(err, data){
